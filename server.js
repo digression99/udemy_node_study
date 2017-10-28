40c0f4ef6280395e6a85f48276182630235640ea
Kim Il Sik <kimilsik@Kims-MacBook.local>
2017-10-28 16:18:56 +0900

    heroku modified.

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000; // for heroku, local
+
+
 var app = express(); // express as a function.
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -92,6 +95,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log("Server is up on port 3000");
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
