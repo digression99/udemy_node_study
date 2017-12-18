4f3e2a223db15250eca672c020684f7149df033a
Kim Il Sik <kimilsik@Kims-MacBook.local>
2017-12-18 15:06:54 +0900

    add tests for POST /users/login

--- mongoose-server/tests/server.test.js
+++ mongoose-server/tests/server.test.js
@@ -262,7 +262,7 @@ describe('POST /users', () => {
                     expect(user).toExist();
                     expect(user.password).toNotBe(password);
                     done();
-                });
+                }).catch(e => done(e));
             });
     });
 
@@ -287,5 +287,68 @@ describe('POST /users', () => {
             .send({email, password})
             .expect(400)
             .end(done);
+    });
+});
+
+describe('POST /users/login', () => {
+    it('should login user and return auth token', (done) => {
+        request(app)
+            .post('/users/login')
+            .send({
+                email : users[1].email,
+                password : users[1].password
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toExist(); // if the token exists.
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                User.findById(users[1]._id).then((user) => {
+                    expect(user.tokens[0]).toInclude({
+                        // should include these.
+                        access : 'auth',
+                        token : res.headers['x-auth']
+                    });
+                    done();
+                }).catch(e => done(e));
+            });
+    });
+
+    it('should reject invalid login.', (done) => {
+        //expect 400
+        // token toNotExist
+        // no token created.
+        request(app)
+            .post('/users/login')
+            .send({
+                email : users[1].email + '1',
+                password : users[1].password
+            })
+            .expect(400)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toNotExist(); // if the token exists.
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                User.findById(users[1]._id).then((user) => {
+                    //expect(user.tokens[0]).toNotExist();
+                    expect(user.tokens.length).toBe(0);
+
+                    // expect(user.tokens[0]).toExclude({
+                    //     // should include these.
+                    //     access : 'auth',
+                    //     token : res.headers['x-auth']
+                    // });
+                    done();
+                }).catch(e => done(e));
+            });
+
     });
 });
