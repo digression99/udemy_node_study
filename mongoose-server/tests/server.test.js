ae4c3dc2b45ddbbf93a35322a50c940edbe91e81
Kim Il Sik <kimilsik@Kims-MacBook.local>
2017-12-19 13:56:09 +0900

    add test for delete /users/me/token

--- mongoose-server/tests/server.test.js
+++ mongoose-server/tests/server.test.js
@@ -352,3 +352,28 @@ describe('POST /users/login', () => {
 
     });
 });
+
+describe('DELETE /users/me/token', () => {
+
+    it('should remove token on logout', (done) => {
+        // DELETE /users/me/token
+        // Set x-auth equal to token
+        // 200 back.
+        // asynchronous assertion.
+        // find user, verify that tokens array has length of zero.
+
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', users[0].tokens[0].token) // set the header.
+            .expect(200)
+            .end((err, res) => {
+                if (err) return done(err);
+                User.findById(users[0]._id)
+                    .then((user) => {
+                        expect(user.tokens.length).toBe(0);
+                        done();
+                })
+                    .catch(e => done(e));
+            });
+    });
+});
