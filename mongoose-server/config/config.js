9f0aaf9a696552df10658a45231615c66054fadc
Kim Il Sik <kimilsik@Kims-MacBook.local>
2017-12-20 15:27:54 +0900

    update config to use json file

--- mongoose-server/config/config.js
+++ mongoose-server/config/config.js
@@ -1,11 +1,25 @@
 let env = process.env.NODE_ENV || 'development'; // used only in heroku.
 // console.log('env **** ', env);
 
-if (env === 'development') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-    process.env.PORT = 3000;
-    // use different database so we don't wipe out existing data.
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+// if you require() json file, then it automatically parsed into javascript object.
+if (env === 'development' || env === 'test') {
+    let config = require('./config.json');
+    //console.log(config);
+    let envConfig = config[env]; // test or development.
+
+    console.log(Object.keys(envConfig)); // returns it an array.
+    Object.keys(envConfig).forEach(key => {
+        // deal env as an array.
+        process.env[key] = envConfig[key];
+    });
 }
+
+//
+// if (env === 'development') {
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+// } else if (env === 'test') {
+//     process.env.PORT = 3000;
+//     // use different database so we don't wipe out existing data.
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+// }
